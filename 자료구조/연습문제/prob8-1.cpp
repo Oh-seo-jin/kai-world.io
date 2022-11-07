a1b6f8ed005a2edce3837d96721d84030112c7bc
ccomrung <106959195+ccomrung@users.noreply.github.com>
2022-11-07 11:05:09 +0900

    modify exericise08

--- 자료구조/연습문제/prob8-1.cpp
+++ 자료구조/연습문제/prob8-1.cpp
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "stack.h"
 #pragma warning (disable:4996)
 #define BUFFER_SIZE 100
 
 int main() {
 	char str[BUFFER_SIZE];
-	scanf("%s", &str);
+	scanf("%s", str);
 
-	int num[BUFFER_SIZE];
+	int num = 0;
 	//stack	
+	
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == '(') {
+			num++;
+			printf("%d ", num);
+			push(num);
+		}
+		else if (str[i] == ')') {
+			printf("%d ", pop());
+		}
+	}
 }
